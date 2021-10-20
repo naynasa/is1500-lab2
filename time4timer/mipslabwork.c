98cc90a8ec9ae4ac744eb3fcd1381ef84eabf9e8
Axel Sparr <asparr@kth.se>
2021-10-20 13:42:55 +0200

    testa utan implicit sätta timern till 0

--- time4timer/mipslabwork.c
+++ time4timer/mipslabwork.c
@@ -14,22 +14,46 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 #include <stdbool.h>
+#define T2CON_ENABLE_BIT 0x8000 //0b1000000000000000
+#define T2CON_PRESCALER_BITS 0x0070//0b0000000001110000 TCKPS<2:0> - we only look at 3 bits (here 111) and 111 <=> 
+
 int mytime = 0x5957;
 int num_ticks = 0;
 char textstring[] = "text, more text, and even more text!";
+bool flag = false;
+uint32_t count = 0;
 
 /* Interrupt Service Routine */
 void user_isr( void )
 {
-  return;
+
+
 }
 
 /* Lab-specific initialization goes here */
 //setting TRISn, where n is the port letter e.g. A,B,C etc. to:
 //1 marks input
 //0 mars output
+
 void labinit( void )
 {
+  //init Timer 2 - 16 bit timer
+  T2CONCLR = T2CON_ENABLE_BIT;
+  uint32_t target_frequency = 10; //10 Hz / our timer timesout 10 times each second
+  uint32_t prescaler = 256; //256 is needed since we need a low frequency 
+  uint32_t pb_clock_frequency = 80E6;
+  uint16_t period = pb_clock_frequency / (prescaler * target_frequency); //set our max value of the timer = 31250
+  T2CON = T2CON_PRESCALER_BITS; //sets prescaler to 256 (see global definition)
+  TMR2 = 0; //set the current number of ticks in our timer to 0
+  PR2 = period; //set the max number of ticks equal to our period
+
+
+  T2CONSET = T2CON_ENABLE_BIT;
+
+  //target_frequency = 80e6 / (256 * 65535) =
+  //perioden är mellan 1-65535 för 16-bitar
+  
+  
   //p -> 0xbf886100
   //*p[0:7] = 1;
   volatile char* port_e_pointer = 0xbf886100; //TRISE
@@ -81,15 +105,27 @@ void labwork( void )
     
     
   }
-  delay( 1000 );
-  time2string( textstring, mytime ); //pass by reference
-  display_string( 3, textstring );
-  display_update();
-  //tick( &mytime );
-  num_ticks++; //increment ticks
-  volatile char* p = 0xbf886110; //PORTE
-  *p = num_ticks; //5 <=> 00000101 <=> 0x05
-  display_image(96, icon);
+
+  if(TMR2 == PR2){ //check if our timer is "full" / has ended
+    TMR2  = 0; //no need to actually set it to 0 since its unsigned so implicitly turns to 0 after overflowing
+    //once every 100ms
+    count++;
+    time2string( textstring, mytime ); //pass by reference
+    display_string( 3, textstring );
+    display_update();
+
+    if(count % 10 == 0){
+      //once every 1 second
+      tick( &mytime );
+      num_ticks++; //increment ticks
+      volatile char* p = 0xbf886110; //PORTE
+      *p = num_ticks; //5 <=> 00000101 <=> 0x05
+
+    }
+    display_image(96, icon);
+  }
+  //delay( 1000 );
+  
 }
 // TRISECLR sätter man en bit i den till 1 sätts motsvarande bit till 0 i TRISE (sätter man en bit till 0 händer inget)
 // TRISESET 1 sätter värdet i TRISET 0 gör inget

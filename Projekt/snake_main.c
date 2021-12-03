f413bfb70bb91cfbe0d225f7abac33ac782f771c
Axel Sparr <asparr@kth.se>
2021-12-03 13:24:42 +0100

    fixa add_square

--- Projekt/snake_main.c
+++ Projekt/snake_main.c
@@ -73,8 +73,8 @@ bool* pixel_to_frame_buffer_position(int x, int y){
 void add_square(int x, int y, int size){
   //set all pixels with x values between x and x+size-1 and y values between y and y+size-1
   int i,j;
-  for(i = x; i < x -1; i++){
-    for(j = y; j < y -1; j++){
+  for(i = x; i < x + size -1; i++){
+    for(j = y; j < y + size -1; j++){
       bool *pixel_pointer = pixel_to_frame_buffer_position(x,y);
       *pixel_pointer = true;
 

8a8956d3091a3a4252ba3d4bd951270283df7a03
Axel Sparr <asparr@kth.se>
2021-12-08 13:13:34 +0100

    hitta vilken del som kraschar programmet 2

--- Projekt/snake_main.c
+++ Projekt/snake_main.c
@@ -160,7 +160,7 @@ int getbtns(void){
 }
 char user_move_direction(){
  
-  int button_status = 2//getbtns(); //0000...0ABCD
+  int button_status = 2;//getbtns(); //0000...0ABCD
   if(button_status != 0){
     bool btn_4_status =  button_status >> 3; //000.0A
     bool btn_3_status =  (button_status >> 2) - (btn_4_status << 1); /// 000AB - 0A<<1 = 000.00B

ca9e9607b4a51c11130f07f3495c9fafb860ac36
Axel Sparr <asparr@kth.se>
2021-12-11 16:52:16 +0100

    fixa problem med collision systemt 15

--- Projekt/snake_main.c
+++ Projekt/snake_main.c
@@ -65,7 +65,7 @@ void add_square(int x, int y, int size){
   int i,j;
   for(i = x; i <= x + size -1; i++){
     for(j = y-size + 1; j <= y; j++){
-      bool *pixel_pointer = pixel_to_frame_buffer_position(i,j);
+      bool* pixel_pointer = pixel_to_frame_buffer_position(i,j);
       *pixel_pointer = true;
 
     }

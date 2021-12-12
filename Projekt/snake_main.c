ef8e531861937745e421e731a833f3538efe18c3
Axel Sparr <asparr@kth.se>
2021-12-12 14:25:46 +0100

    BASIC PROJECT DONE + BETTER STRUCTURED!

--- Projekt/snake_main.c
+++ Projekt/snake_main.c
@@ -31,15 +31,15 @@ uint16_t rand_seed = 1;
 #define MAX_NUMBER_OF_POSSIBLE_BLOCKS (SCREEN_HEIGHT*SCREEN_WIDTH)/(BLOCK_SIZE*BLOCK_SIZE)
 
 
-/**/
+/*the basic building block of our game - a square of size BLOCK_SIZE with x0,y0 being the bottom left most pixel in the square*/
 typedef struct {
   uint8_t x0; /*value range: 0-128, lower left hand corner x coordinate*/
   uint8_t y0; /*value range: 0-32, lower left hand corner y coordinate*/
-  //uint8_t size; /*value range: 1-32 max for uint8_t is 255, the size of the square*/
+//uint8_t size; /*value range: 1-32 max for uint8_t is 255, the size of the square*/
 
 }Block;
 
-/**/
+/*the snake the user controlls*/
 typedef struct {
   uint16_t num_blocks; /*number of blocks contained at the pointer location / that belong to the snake*/
   int num_apples_eaten; /*could be unsigned but noone is gonna collect over 2 million apples so we should be fine*/
@@ -53,7 +53,7 @@ typedef struct {
   Block block; /*the square that symbolizes the apple*/
 }Apple;
 
-/*declare global data variables*/
+/*declare global data variables - "instances" of our structs*/
 Snake snake;
 Apple apple;
 
@@ -65,14 +65,8 @@ int main(void) {
     T2CONSET = T2CON_ENABLE_BIT;
   }
 
-  /*initializations*/ 
-	init_controller();
-	display_init();
-  init_timer();
-  init_LEDs();
-  init_buttons_switches();
-
-  /*todo make random - so you don't start at the same place each time*/
+  void init_snake(){
+    /*todo make random? - so you don't start at the same place each time*/
   
   snake.blocks_array[0] = (Block) {40,15};
   snake.blocks_array[1] = (Block) {40-1*BLOCK_SIZE,15};
@@ -80,6 +74,7 @@ int main(void) {
   snake.blocks_array[3] = (Block) {40-3*BLOCK_SIZE,15};
   snake.blocks_array[4] = (Block) {40-4*BLOCK_SIZE,15};
   
+  /*
   snake.blocks_array[5] = (Block) {40-5*BLOCK_SIZE,15};
   snake.blocks_array[6] = (Block) {40-6*BLOCK_SIZE,15};
   snake.blocks_array[7] = (Block) {40-7*BLOCK_SIZE,15};
@@ -87,41 +82,35 @@ int main(void) {
   snake.blocks_array[9] = (Block) {40-9*BLOCK_SIZE,15};
   snake.blocks_array[10] = (Block) {40-10*BLOCK_SIZE,15};
   snake.blocks_array[11] = (Block) {40-11*BLOCK_SIZE,15};
-  snake.blocks_array[12] = (Block) {40-12*BLOCK_SIZE,15};
- /*
-  snake.blocks_array[0] = (Block) {10+4*BLOCK_SIZE,15};
-  snake.blocks_array[1] = (Block) {10+3*BLOCK_SIZE,15};
-  snake.blocks_array[2] = (Block) {10+2*BLOCK_SIZE,15};
-  snake.blocks_array[3] = (Block) {10+1*BLOCK_SIZE,15};
-  snake.blocks_array[4] = (Block) {10+0*BLOCK_SIZE,15};
 */
+
   snake.facing_direction = 'R'; //set the snake to always start going right
   snake.num_blocks = 13;//sizeof(blocks) / sizeof(blocks[0]);
   snake.num_apples_eaten = 0;
   apple.block = (Block) {128/2,15};
   snake.prev_tail = (Block) {40,15};
+  }
+
+  /*initializations*/ 
+	init_controller();
+	display_init();
+  init_timer();
+  init_LEDs();
+  init_buttons_switches();
 
 
 
-
+  init_snake();
   
 	start_timer();
 
 
 	while( 1 )
 	{
-	  game_main(); /* Do lab-specific things again and again */
+	  /*our "game loop" just to keep the program alive - all calculations are done in render_frame()*/
 	}
 	return 0;
 }
-
-/*
-eat_apple
-check_collide - kollar om han kolliderat med väggen eller sin svans
-move_snake
-game_over
-display_highscore
-*/
 
 
 //each block is 32*128/4 = 4096/4 = 1024 bits
@@ -133,15 +122,6 @@ bool* pixel_to_frame_buffer_position(int x, int y){
 
   return &frame_buffer[page][byte_index][bit_index];
 }
-bool get_pixel_value_at_frame_buffer_position(uint16_t x, uint16_t y){
-  uint16_t page = y / 8; // since we have 4 blocks of 8 height each
-  uint16_t byte_index = x;
-  uint16_t bit_index = y % 8; //each byte is 8 bits so modulo gives us how "far into" the block we are
-
-  return frame_buffer[page][byte_index][bit_index];
-}
-
-
 
 /*x,y mark starting points of the square (lower left hand corner)*/
 void add_square(int x, int y, int size){
@@ -171,94 +151,76 @@ void set_all_pixels_black(){
     display_buffer();
 }
 
-
-/* Our game loop - This function is called repetitively from the main program 
-we imagine each pixel has an x,y value with 0,0 being in the bottom left corner
-*/
-void game_main( void ){
-
-//wait_x_ms()
-//calculate_frame();
-
-}
-
-
-//moves the snakes head forward if no command is given or in another direction if the user presses one of the buttons
-//if the user tries to move 180 degrees from current direction or presses multiple buttons the command is ignored
-//called each frame
-//updates position of the snakes head block and snake.facing_direction
-void move_head(){
-  /*helper that calculates how much x should be updated depending on the direction*/
-  int x_offset_from_dir(Direction dir){
-    if(dir == 'L' || dir == 'R'){
-      return (dir == 'L') ? -SPEED : SPEED; // L = - and R = +
-    }else{
-      return 0;
-    }
-  }
-  /*helper that calculates how much y should be updated depending on the direction*/
-  int y_offset_from_dir(Direction dir){
-    if(dir == 'U' || dir == 'D'){
-      return (dir == 'U') ? -SPEED : SPEED; // U = - and D = +
-    }else{
-      return 0;
-    }
-  }
-  /*helper that calculates if the user is trying to turn left when going right or up when going down etc. (180 degrees)*/
-  bool user_turning_180_degrees(Direction dir){
-    switch (snake.facing_direction){
-    case 'U':
-      return dir == 'D';
-      
-    case 'D':
-      return dir == 'U';
-
-    case 'L':
-      return dir == 'R';
-      
-    case 'R':
-      return dir == 'L';
-  
-    default:
-      return false;
-    }
-  }
-  Direction user_move_dir = user_move_direction();
-  int i;
-  if(user_move_dir == 'N' || user_turning_180_degrees(user_move_dir)){
-    /*no command or invalid command - just move the snake forward*/
-    int x_add = x_offset_from_dir(snake.facing_direction);
-    int y_add = y_offset_from_dir(snake.facing_direction);
-    
-    //update the head coordinates
-    snake.blocks_array[0].x0 += x_add;
-    snake.blocks_array[0].y0 += y_add;
-
-    //snake.blocks_array[0].x0 %= SCREEN_WIDTH; //-uncomment to make the snake move through walls
-    //snake.blocks_array[0].y0 %= SCREEN_HEIGHT;
-
-  }else{
-    /*a valid command is sent - move the snake in the direction the user wants*/
-
-    int x_add = x_offset_from_dir(user_move_dir);
-    int y_add = y_offset_from_dir(user_move_dir);
-    
-    //update the head coordinates
-    snake.blocks_array[0].x0 += x_add;
-    snake.blocks_array[0].y0 += y_add;
-
-    //snake.blocks_array[0].x0 %= SCREEN_WIDTH;-uncomment to make the snake move through walls
-    //snake.blocks_array[0].y0 %= SCREEN_HEIGHT;
-
-    //update the direction the snake is facing
-    snake.facing_direction = user_move_dir;
-  }
-  
-
-}
 //makes the snake "slither" by first moving the head then moving each block into the position the following block was in before
 //also assigns snake_tail
 void move_snake(){
+  //moves the snakes head forward if no command is given or in another direction if the user presses one of the buttons
+  //if the user tries to move 180 degrees from current direction or presses multiple buttons the command is ignored
+  //called each frame
+  //updates position of the snakes head block and snake.facing_direction
+  void move_head(){
+    /*helper that calculates how much x should be updated depending on the direction*/
+    int x_offset_from_dir(Direction dir){
+      if(dir == 'L' || dir == 'R'){
+        return (dir == 'L') ? -SPEED : SPEED; // L = - and R = +
+      }else{
+        return 0;
+      }
+    }
+    /*helper that calculates how much y should be updated depending on the direction*/
+    int y_offset_from_dir(Direction dir){
+      if(dir == 'U' || dir == 'D'){
+        return (dir == 'U') ? -SPEED : SPEED; // U = - and D = +
+      }else{
+        return 0;
+      }
+    }
+    /*helper that calculates if the user is trying to turn left when going right or up when going down etc. (180 degrees)*/
+    bool user_turning_180_degrees(Direction dir){
+      switch (snake.facing_direction){
+      case 'U':
+        return dir == 'D';
+        
+      case 'D':
+        return dir == 'U';
+
+      case 'L':
+        return dir == 'R';
+        
+      case 'R':
+        return dir == 'L';
+    
+      default:
+        return false;
+      }
+    }
+    Direction user_move_dir = user_move_direction();
+    int i;
+    if(user_move_dir == 'N' || user_turning_180_degrees(user_move_dir)){
+      /*no command or invalid command - just move the snake forward*/
+      int x_add = x_offset_from_dir(snake.facing_direction);
+      int y_add = y_offset_from_dir(snake.facing_direction);
+      
+      //update the head coordinates
+      snake.blocks_array[0].x0 += x_add;
+      snake.blocks_array[0].y0 += y_add;
+
+    }else{
+      /*a valid command is sent - move the snake in the direction the user wants*/
+
+      int x_add = x_offset_from_dir(user_move_dir);
+      int y_add = y_offset_from_dir(user_move_dir);
+      
+      //update the head coordinates
+      snake.blocks_array[0].x0 += x_add;
+      snake.blocks_array[0].y0 += y_add;
+
+      //update the direction the snake is facing
+      snake.facing_direction = user_move_dir;
+    }
+    
+
+  }
   //copy snake blocks at t-1 - we're not interested in the entire snake just the blocks so copy those
   Block old_blocks[snake.num_blocks];// = snake.blocks_array;
 
@@ -287,20 +249,13 @@ void move_snake(){
   
 
 }
+/*called when the player loses the game - just infinitly prints game over and the score*/
 void game_over(){
-  /*
-  char[] int_to_string(int n){
-    
-  }
-  */
   set_all_pixels_black();
   display_buffer();
-  //itoa(snake.num_apples_eaten,score_string,10);
   
-  char score_string[18];
-  
-
-  sprintf(score_string, "score: %d",snake.num_apples_eaten);
+  char score_string[18]; //holds the score string
+  sprintf(score_string, "score: %d!",snake.num_apples_eaten); //format the score string
 
   while (true)
   {
@@ -353,15 +308,10 @@ void check_collision(){
     return false;
     
   }
-  /*
-  //helper that returns whether or not the pixel we are checking is inside an apple
-  bool check_pixel_is_apple(uint16_t x,uint16_t y){
-    return x >= apple.block.x0 && x <= (apple.block.x0 + BLOCK_SIZE -1) && y >= apple.block.y0 && y <= (apple.block.y0 - BLOCK_SIZE +1); 
+  /*returns whether or not the head is on an apple*/
+  bool is_apple(){
+    return snake.blocks_array[0].x0 == apple.block.x0 && snake.blocks_array[0].y0 == apple.block.y0;
   }
-  */
- bool is_apple(){
-   return snake.blocks_array[0].x0 == apple.block.x0 && snake.blocks_array[0].y0 == apple.block.y0;
- }
       bool head_is_outside_screen = check_outside_screen();
       bool head_is_apple = is_apple();
       bool head_collided_with_tail = check_tail();
@@ -373,8 +323,6 @@ void check_collision(){
 
   
 }
-
-
 
 
 /*Render a new frame - called when timer ticks over*/
@@ -391,9 +339,6 @@ void render_frame() {
   move_snake();
  
   
-  
-  
-  
   //send the snake to the buffer - render snake
   for(i = 0; i<snake.num_blocks; i++){
     //iterates over each block in the snake
@@ -405,16 +350,7 @@ void render_frame() {
   //send the apple to the buffer
   add_square(apple.block.x0,apple.block.y0,BLOCK_SIZE); //write the apple
   
- /*
- //{{10,15}, {10,15-BLOCK_SIZE}, {10,15-2*BLOCK_SIZE}}
-  add_square(10,15,BLOCK_SIZE);
-  add_square(10,15-BLOCK_SIZE,BLOCK_SIZE);
-  add_square(10,15-2*BLOCK_SIZE,BLOCK_SIZE);
-  */
   display_buffer();
-
-
-
   reset_isr();
 
 }

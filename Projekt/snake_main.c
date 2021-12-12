8326f0239a0c91e41ad5197622f90a2fb4b8d4ae
Axel Sparr <asparr@kth.se>
2021-12-12 13:39:50 +0100

    fixa problem med collision systemt 34

--- Projekt/snake_main.c
+++ Projekt/snake_main.c
@@ -75,10 +75,10 @@ int main(void) {
   /*todo make random - so you don't start at the same place each time*/
   
   snake.blocks_array[0] = (Block) {40,15};
-  snake.blocks_array[1] = (Block) {40-1*BLOCK_SIZE,15};
-  snake.blocks_array[2] = (Block) {40-2*BLOCK_SIZE,15};
-  snake.blocks_array[3] = (Block) {40-3*BLOCK_SIZE,15};
-  snake.blocks_array[4] = (Block) {40-4*BLOCK_SIZE,15};
+  //snake.blocks_array[1] = (Block) {40-1*BLOCK_SIZE,15};
+  //snake.blocks_array[2] = (Block) {40-2*BLOCK_SIZE,15};
+  //snake.blocks_array[3] = (Block) {40-3*BLOCK_SIZE,15};
+  //snake.blocks_array[4] = (Block) {40-4*BLOCK_SIZE,15};
   
  /*
   snake.blocks_array[0] = (Block) {10+4*BLOCK_SIZE,15};
@@ -88,7 +88,7 @@ int main(void) {
   snake.blocks_array[4] = (Block) {10+0*BLOCK_SIZE,15};
 */
   snake.facing_direction = 'R'; //set the snake to always start going right
-  snake.num_blocks = 5;//sizeof(blocks) / sizeof(blocks[0]);
+  snake.num_blocks = 1;//sizeof(blocks) / sizeof(blocks[0]);
   snake.num_apples_eaten = 0;
   apple.block = (Block) {128/2,15};
   snake.prev_tail = (Block) {40,15};
